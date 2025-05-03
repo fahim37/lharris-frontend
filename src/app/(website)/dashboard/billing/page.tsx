4675ac8fd5e1cfd0fef4a209af1619244980ae52
SIRsahed <sahedrahmanshakil@gmail.com>
2025-05-03 12:37:05 +0600

    billing

--- src/app/(website)/dashboard/billing/page.tsx
+++ src/app/(website)/dashboard/billing/page.tsx
@@ -19,6 +19,7 @@ export default function BillingPage() {
   const TOKEN = session?.data?.accessToken
 
   const [page, setPage] = useState(1)
+  const limit = 1;
   const [currentPage, setCurrentPage] = useState(1)
   const [selectedPayment, setSelectedPayment] = useState<any>(null)
   const [isModalOpen, setIsModalOpen] = useState(false)
@@ -31,7 +32,7 @@ export default function BillingPage() {
   const { data } = useQuery({
     queryKey: ["paymentDetails", page],
     queryFn: async () => {
-      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/payments/user-payment/${userInfo?.id}`, {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/payments/user-payment/${userInfo?.id}?page=${page}&limit=${limit}`, {
         headers: {
           Authorization: `Bearer ${TOKEN}`,
         },
@@ -76,68 +77,84 @@ export default function BillingPage() {
     >
       <div className="space-y-6">
         <div className="rounded-md border overflow-hidden">
-          <div className="overflow-x-auto">
-            <Table className="min-w-[800px]">
-              <TableHeader>
-                <TableRow>
-                  <TableHead className="w-[50px] text-center pl-10">Transaction ID</TableHead>
-                  <TableHead className="text-center">Date</TableHead>
-                  <TableHead className="text-center">Visit Time</TableHead>
-                  <TableHead className="text-center">Amount</TableHead>
-                  <TableHead className="text-center">Type</TableHead>
-                  <TableHead className="text-center">Status</TableHead>
-                  <TableHead className="text-center">Actions</TableHead>
-                </TableRow>
-              </TableHeader>
-              <TableBody>
-                {data?.data?.map((item: any) => (
-                  <TableRow key={item.id} className="text-center">
-                    <TableCell className="font-medium pl-10 ">{item.transactionId}</TableCell>
-                    <TableCell>
-                      {new Date(item.paymentDate).toLocaleDateString("en-US", {
-                        year: "numeric",
-                        month: "short",
-                        day: "numeric",
-                      })}
-                    </TableCell>
-                    <TableCell>
-                      {new Date(item.paymentDate).toLocaleTimeString("en-US", {
-                        hour: "numeric",
-                        minute: "2-digit",
-                        hour12: true,
-                      })}
-                    </TableCell>
-                    <TableCell>{item.formattedAmount}</TableCell>
-                    <TableCell className="capitalize">{item?.status}</TableCell>
-                    <TableCell className="max-w-[200px]">
-                      <span
-                        className={cn("px-2 py-1 rounded-full text-xs font-medium", {
-                          "bg-[#B3E9C9] text-[#033618]": item?.status === "completed",
-                          "bg-[#FFD6D6] text-[#5C0000]": item?.status === "failed",
-                          "bg-[#FFF3CD] text-[#856404]": item?.status === "pending",
-                          "bg-[#CCE5FF] text-[#004085]": item?.status === "refunded",
-                        })}
-                      >
-                        {item?.status === "completed" ? "Paid" : item?.status}
-                      </span>
-                    </TableCell>
-                    <TableCell className="text-right">
-                      <div className="flex justify-center gap-2">
-                        <Button variant="ghost" size="icon" onClick={() => handleViewPaymentDetails(item)}>
-                          <Eye className="h-4 w-4" />
-                          <span className="sr-only">View payment details</span>
-                        </Button>
-                        <Button variant="ghost" size="icon" onClick={() => handleDownloadPaymentDetails(item)}>
-                          <Download className="h-4 w-4" />
-                          <span className="sr-only">Download payment receipt</span>
-                        </Button>
-                      </div>
-                    </TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </div>
+          {data?.data?.length === 0 ? (
+            <div className="p-4 text-center">
+              <p className="text-sm text-muted-foreground">No payments found.</p>
+            </div>
+          )
+            : (
+
+              <div className="overflow-x-auto">
+                <Table className="min-w-[800px]">
+                  <TableHeader>
+                    <TableRow>
+                      <TableHead className="w-[50px] text-center pl-10">Transaction ID</TableHead>
+                      <TableHead className="text-center">Date</TableHead>
+                      <TableHead className="text-center">Visit Time</TableHead>
+                      <TableHead className="text-center">Amount</TableHead>
+                      <TableHead className="text-center">Type</TableHead>
+                      <TableHead className="text-center">Status</TableHead>
+                      <TableHead className="text-center">Actions</TableHead>
+                    </TableRow>
+                  </TableHeader>
+                  <TableBody>
+                    {data?.data?.map((item: any) => (
+                      <TableRow key={item.id} className="text-center">
+                        <TableCell className="font-medium pl-10 ">{item.transactionId}</TableCell>
+                        <TableCell>
+                          {new Date(item.paymentDate).toLocaleDateString("en-US", {
+                            year: "numeric",
+                            month: "short",
+                            day: "numeric",
+                          })}
+                        </TableCell>
+                        <TableCell>
+                          {new Date(item.paymentDate).toLocaleTimeString("en-US", {
+                            hour: "numeric",
+                            minute: "2-digit",
+                            hour12: true,
+                          })}
+                        </TableCell>
+                        <TableCell>{item.formattedAmount}</TableCell>
+                        <TableCell className="capitalize">{item?.status}</TableCell>
+                        <TableCell className="max-w-[200px]">
+                          <span
+                            className={cn("px-2 py-1 rounded-full text-xs font-medium", {
+                              "bg-[#B3E9C9] text-[#033618]": item?.status === "completed",
+                              "bg-[#FFD6D6] text-[#5C0000]": item?.status === "failed",
+                              "bg-[#FFF3CD] text-[#856404]": item?.status === "pending",
+                              "bg-[#CCE5FF] text-[#004085]": item?.status === "refunded",
+                            })}
+                          >
+                            {item?.status === "completed" ? "Paid" : item?.status}
+                          </span>
+                        </TableCell>
+                        <TableCell className="text-right">
+                          <div className="flex justify-center gap-2">
+                            <Button variant="ghost" size="icon" onClick={() => handleViewPaymentDetails(item)}>
+                              <Eye className="h-4 w-4" />
+                              <span className="sr-only">View payment details</span>
+                            </Button>
+                            <Button variant="ghost" size="icon" onClick={() => handleDownloadPaymentDetails(item)}>
+                              <Download className="h-4 w-4" />
+                              <span className="sr-only">Download payment receipt</span>
+                            </Button>
+                          </div>
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+                <PaginationComponent
+                  totalItems={data?.meta?.totalItems}
+                  itemsPerPage={data?.meta?.itemsPerPage}
+                  currentPage={data?.meta?.currentPage}
+                  totalPages={data?.meta?.totalPages}
+                  onPageChange={handlePageChange}
+                />
+              </div>
+            )
+          }
         </div>
 
         <PaymentDetailsModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} payment={selectedPayment} />
